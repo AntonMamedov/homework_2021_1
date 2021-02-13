86adf86b88b39497af1751f1a4e85e358007a110
AntonMamedov <tokha.mamiedov@gmail.com>
2021-02-13 21:17:27 +0300

    JSDoc, use strict

--- source/sorting.js
+++ source/sorting.js
@@ -1,19 +1,26 @@
+"use strict";
+/**
+ * Функция принимает на вход массив plain-объектов и массив имён свойств,
+ * по которым необходимо отсортировать массив объектов,
+ * и реализует устойчивую сортировку этого массива в порядке возрастания
+ * (строки сортируются лексикографически, числа — в порядке возрастания).
+ *
+ * @param {Array} objArr - исходный массив, который сортируется
+ * @param {Array} propName - массив имен свойств, по которым проводится сортировка
+ *
+ * @throws {TypeError} - выбрасывает ошибку, если на вход приходят не массивы
+ */
 let sorting = (objArr, propName) => {
-    if (Array.isArray(objArr) && Array.isArray(propName)) {
-        objArr.sort((left, right) => {
-            for (let prop of propName) {
-                //Не получиься применить тернарный оператор, потому что
-                //отрицанием < является >= а в данном случае = должно обрабатываться отдельно
-                //Данный метод итерации по массиву представляется мне наиболее лаконичным, не вижу смысла использовать форич
-                if (left[prop] < right[prop])
-                    return -1;
-                else if (left[prop] > right[prop])
-                    return 1;
-            }
-            return 0;
-        });
-        return objArr;
-    } else {
-        throw "non-volatile data";
+    if (!Array.isArray(objArr) || !Array.isArray(propName)) {
+        throw new TypeError("non-volatile data");
     }
+    return objArr.sort((left, right) => {
+        for (let prop of propName) {
+            if (left[prop] < right[prop])
+                return -1;
+            else if (left[prop] > right[prop])
+                return 1;
+        }
+        return 0;
+    });
 }
